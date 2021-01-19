a0bf27a38c7fadb618d993906eb083f18c3057bb
Jose Mejia <josemejia247@gmail.com>
2021-01-19 17:39:33 -0500

    Prepare for initial Heroku push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,36 @@
 class User < ApplicationRecord
+
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    after_initialize :ensure_session_token
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+
+    end
+
+    def password=(password)
+
+    end
+
+    def is_password?(password)
+
+    end
+
+    def reset_session_token!
+
+    end
+
+    private
+
+    def ensure_session_token 
+
+    end
+
+    def generate_unique_session_token
+
+    end
+
 end
