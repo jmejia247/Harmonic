658ece540c322432c113f192ccdb9e4f9417f0eb
Jose Mejia <josemejia247@gmail.com>
2021-04-27 13:20:47 -0400

    styling

--- frontend/components/filterBar/filterBar.jsx
+++ frontend/components/filterBar/filterBar.jsx
@@ -9,23 +9,58 @@ class FilterBar extends React.Component {
   render() {
     return (
       <div className="filterbar">
-          <div className='bar1'>
-          <Link to='/' className='filter-navbar'>Guitars</Link>
-          <Link to='/' className='filter-navbar'>Pedals and Amplifiers</Link>
-          <Link to='/' className='filter-navbar'>Keyboards and Synths</Link>
-          <Link to='/' className='filter-navbar'>Recording Gear</Link>
-          <Link to='/' className='filter-navbar'>Drums</Link>
-          <Link to='/' className='filter-navbar'>DJ and audio gear</Link>
-          <Link to='/' className='filter-navbar'>More Categories</Link>
-          </div>
-          <div className='bar2'>
-          <Link to='/' className='filter-navbar2'>Brands</Link>
+        <div className="bar1">
+          <h3 to="/" className="filter-navbar">
+             Find Guitars
+          </h3>
+          <h3 to="/" className="filter-navbar">
+            Pedals and Amplifiers
+          </h3>
+          <h3 to="/" className="filter-navbar">
+            Keyboards and Synths
+          </h3>
+          <h3 to="/" className="filter-navbar">
+            Recording Gear
+          </h3>
+          <h3 to="/" className="filter-navbar">
+            Drums
+          </h3>
+          <h3 to="/" className="filter-navbar">
+            Bass'
+          </h3>
+          <h3 to="/" className="filter-navbar">
+            and More
+          </h3>
+        </div>
+        <div className="bar2">
+          {/* <Link to='/' className='filter-navbar2'>Brands</Link>
           <Link to='/' className='filter-navbar2'>News</Link>
-          <Link to='/' className='filter-navbar2'>Shops</Link>
-          <Link to='/' className='filter-navbar2'>Github</Link>
-          <Link to='/' className='filter-navbar2'>LinkedIn</Link>
-          <Link to='/' className='filter-navbar2'>Resume</Link>
-          </div>
+          <Link to='/' className='filter-navbar2'>Shops</Link> */}
+          <a
+            href="https://github.com/jmejia247"
+            target="_blank"
+            rel="noreferrer"
+            className="filter-navbar2"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jose-mejia-531907206/"
+            target="_blank"
+            rel="noreferrer"
+            className="filter-navbar2"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://angel.co/u/jose-mejia-15"
+            target="_blank"
+            rel="noreferrer"
+            className="filter-navbar2"
+          >
+            AngelList
+          </a>
+        </div>
       </div>
     );
   }
