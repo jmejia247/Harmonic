9b21a57c190d075c3620eca85fede7a650ea3062
Jose Mejia <josemejia247@gmail.com>
2021-05-17 14:43:53 -0400

    persist log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,9 @@ class ApplicationController < ActionController::Base
     private
 
     def current_user
-        return nil unless session[:session_token]
+        # return nil unless session[:session_token]
         @current_user ||= User.find_by(session_token: session[:session_token])
+        return @current_user
     end
 
     def logged_in?
--- frontend/harmonic.jsx
+++ frontend/harmonic.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-// import { signup, login, logout } from './actions/session_actions';
 import { requestListing, requestListings } from './actions/listing_actions';
 import { fetchListings, fetchListing } from './util/listing_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // window.signup = signup;
-    // window.login = login;
-    // window.logout = logout;
     window.requestListing = requestListing;
     window.requestListings = requestListings;
     window.fetchListing = fetchListing;
     window.fetchListings = fetchListings;
     let store;
-    let preloadedState = undefined;
-    if (window.current_user) {
-        preloadedState = {
-            session: {
-                currentUser: window.currentUser
-            }
+    if (window.currentUser) {
+        const { currentUser } = window;
+        const { id } = currentUser;
+        const preloadedState = {
+          entities: {
+            users: {
+              [id]: currentUser,
+            },
+          },
+          session: {
+            id: currentUser.id,
+            email: currentUser.email
+          }
         };
         store = configureStore(preloadedState);
         delete window.currentUser;
