8e11cb5e5e6b7ab5bbfd33ff6ee6d9c8b271ca7a
Jose Mejia <josemejia247@gmail.com>
2024-01-26 16:39:17 -0500

    added procfile

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Harmonic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
     config.active_record.legacy_connection_handling = false
 
     # Settings in config/environments/* take precedence over those specified here.
